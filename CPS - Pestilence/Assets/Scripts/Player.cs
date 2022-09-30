774bc78b798a5ec6015f60b95af847216d19be0d
gstewar7 <gstewar7@calstatela.edu>
2022-09-29 21:21:33 -0700

    I don't really remember what I did
    lol

--- CPS - Pestilence/Assets/Scripts/Player.cs
+++ CPS - Pestilence/Assets/Scripts/Player.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float currentHealth = 100;
-
+    private Animator anim;
+    private float temp;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(currentHealth);
+        
+        temp = currentHealth;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(currentHealth < temp)
+        {
+            anim.Play("Hit");
+            temp = currentHealth;
+        }
     }
 }
